48efa10c12ed1646bfa7aac70298eacb3e6b7618
Jason <jason.johnson.081@gmail.com>
2022-12-05 14:32:44 +0000

    trying to add a function

--- terraform/function.tf
+++ terraform/function.tf
@@ -23,3 +23,31 @@ resource "azurerm_linux_function_app" "main" {
     type = "SystemAssigned"
   }
 }
+
+resource "azurerm_function_app_function" "example" {
+  name            = "http-trigger-test"
+  function_app_id = azurerm_linux_function_app.main.id
+  language        = "Node"
+  test_data = jsonencode({
+    "name" = "Azure"
+  })
+  config_json = jsonencode({
+    "bindings" = [
+      {
+        "authLevel" = "function"
+        "direction" = "in"
+        "methods" = [
+          "get",
+          "post",
+        ]
+        "name" = "req"
+        "type" = "httpTrigger"
+      },
+      {
+        "direction" = "out"
+        "name"      = "$return"
+        "type"      = "http"
+      },
+    ]
+  })
+}
