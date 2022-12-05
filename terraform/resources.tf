670e7f6a275c9fc7667af927ca7d0d0ef468844a
Jason <jason.johnson.081@gmail.com>
2022-12-05 15:08:41 +0000

    add event hub

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -35,3 +35,31 @@ resource "azurerm_service_plan" "main" {
   os_type             = "Linux"
   sku_name            = "Y1"
 }
+
+data "namep_azure_name" "ns" {
+  name     = "main"
+  location = "westeurope"
+  type     = "azurerm_eventhub_namespace"
+}
+
+resource "azurerm_eventhub_namespace" "main" {
+  name                = data.namep_azure_name.ns.result
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+  capacity            = 1
+}
+
+data "namep_azure_name" "eh" {
+  name     = "main"
+  location = "westeurope"
+  type     = "azurerm_eventhub"
+}
+
+resource "azurerm_eventhub" "main" {
+  name                = data.namep_azure_name.eh.result
+  namespace_name      = azurerm_eventhub_namespace.rg.name
+  resource_group_name = azurerm_resource_group.rg.name
+  partition_count     = 2
+  message_retention   = 1
+}
