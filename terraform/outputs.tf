2d97e356ab9bbb8ac6c2fbaa6ee328b3f9b699a4
Jason <jason.johnson.081@gmail.com>
2022-12-05 13:34:26 +0000

    fixed outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,19 @@
 output "rg_name" {
   description = "Name of project resource group"
-  value       = azurerm_resource_group.rg.name
+  value       = azurerm_resource_group.main.name
+}
+
+output "sa_name" {
+  description = "Name of project storage account"
+  value       = azurerm_storage_account.main.name
+}
+
+output "sp_name" {
+  description = "Name of project service plan"
+  value       = azurerm_service_plan.main.name
+}
+
+output "fun_name" {
+  description = "Name of project azure function app"
+  value       = azurerm_linux_function_app.main.name
 }
