fa8cd7f04593173a844c3b7cba22dca1b9e2b9fe
Jason <jason.johnson.081@gmail.com>
2022-12-05 12:40:03 +0000

    add namep provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,10 @@
 terraform {
   backend "azurerm" { }
 }
+
+provider "namep" {
+  slice_string     = "MOBI TEST"
+  default_location = "westeurope"
+  default_nodash_name_format = "#{SLUG}#{TOKEN_1}#{TOKEN_2}#{SHORT_LOC}#{NAME}"
+  default_resource_name_format = "#{SLUG}-#{TOKEN_1}-#{TOKEN_2}-#{SHORT_LOC}-#{NAME}"
+}
