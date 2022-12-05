9acca76d86deba6d9af93a539fa7d73f8dc95e4e
Jason <jason.johnson.081@gmail.com>
2022-12-05 13:10:07 +0000

    add azurerm requirements

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,7 +1,11 @@
 terraform {
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.34"
+    }
     namep = {
-      source = "jason-johnson/namep"
+      source  = "jason-johnson/namep"
       version = "~> 1.0"
     }
   }
