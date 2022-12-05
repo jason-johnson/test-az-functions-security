023438e740e8b0ce4cfec959cc2eaed7e2000038
Jason <jason.johnson.081@gmail.com>
2022-12-05 15:44:12 +0000

    remove test function

--- terraform/function.tf
+++ terraform/function.tf
@@ -23,31 +23,3 @@ resource "azurerm_linux_function_app" "main" {
     type = "SystemAssigned"
   }
 }
-
-resource "azurerm_function_app_function" "example" {
-  name            = "http-trigger-test"
-  function_app_id = azurerm_linux_function_app.main.id
-  language        = "TypeScript"
-  test_data = jsonencode({
-    "name" = "Azure"
-  })
-  config_json = jsonencode({
-    "bindings" = [
-      {
-        "authLevel" = "function"
-        "direction" = "in"
-        "methods" = [
-          "get",
-          "post",
-        ]
-        "name" = "req"
-        "type" = "httpTrigger"
-      },
-      {
-        "direction" = "out"
-        "name"      = "$return"
-        "type"      = "http"
-      },
-    ]
-  })
-}
