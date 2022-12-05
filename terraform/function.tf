936514f85a88a1ac3c5aeb1773ea215284a3f3c1
Jason <jason.johnson.081@gmail.com>
2022-12-05 15:47:47 +0000

    just completely remove functions

--- terraform/function.tf
+++ terraform/function.tf
@@ -1,25 +1,25 @@
-data "namep_azure_name" "fun" {
-  name     = "main"
-  location = "westeurope"
-  type     = "azurerm_function_app"
-}
-
-resource "azurerm_linux_function_app" "main" {
-  name                = data.namep_azure_name.fun.result
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-
-  storage_account_name       = azurerm_storage_account.main.name
-  storage_account_access_key = azurerm_storage_account.main.primary_access_key
-  service_plan_id            = azurerm_service_plan.main.id
-
-  site_config {
-    application_stack {
-      node_version = 18
-    }
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-}
+#data "namep_azure_name" "fun" {
+#  name     = "main"
+#  location = "westeurope"
+#  type     = "azurerm_function_app"
+#}
+#
+#resource "azurerm_linux_function_app" "main" {
+#  name                = data.namep_azure_name.fun.result
+#  resource_group_name = azurerm_resource_group.rg.name
+#  location            = azurerm_resource_group.rg.location
+#
+#  storage_account_name       = azurerm_storage_account.main.name
+#  storage_account_access_key = azurerm_storage_account.main.primary_access_key
+#  service_plan_id            = azurerm_service_plan.main.id
+#
+#  site_config {
+#    application_stack {
+#      node_version = 18
+#    }
+#  }
+#
+#  identity {
+#    type = "SystemAssigned"
+#  }
+#}
