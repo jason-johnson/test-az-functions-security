1531b9f236f6eb34b324a6cb5c4b56392f5957b0
Jason <jason.johnson.081@gmail.com>
2022-12-05 13:04:05 +0000

    add resource group

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,14 @@ provider "namep" {
   default_nodash_name_format = "#{SLUG}#{TOKEN_1}#{TOKEN_2}#{SHORT_LOC}#{NAME}"
   default_resource_name_format = "#{SLUG}-#{TOKEN_1}-#{TOKEN_2}-#{SHORT_LOC}-#{NAME}"
 }
+
+data "namep_azure_name" "rg" {
+  name     = "test"
+  location = "westeurope"
+  type     = "azurerm_resource_group"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = data.namep_azure_name.rg.result
+  location = "West Europe"
+}
