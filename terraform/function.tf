9db216d8bcf0b559a2bf84fe21064d2185040ecc
Jason <jason.johnson.081@gmail.com>
2022-12-12 18:01:22 +0000

    add application insights

--- terraform/function.tf
+++ terraform/function.tf
@@ -1,3 +1,31 @@
+data "namep_azure_name" "ws" {
+  name     = "main"
+  location = "westeurope"
+  type     = "azurerm_log_analytics_workspace"
+}
+
+resource "azurerm_log_analytics_workspace" "main" {
+  name                = data.namep_azure_name.ws.result
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+data "namep_azure_name" "ai" {
+  name     = "main"
+  location = "westeurope"
+  type     = "azurerm_application_insights"
+}
+
+resource "azurerm_application_insights" "main" {
+  name                = data.namep_azure_name.ai.result
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  workspace_id        = azurerm_log_analytics_workspace.main.id
+  application_type    = "Node.JS"
+}
+
 data "namep_azure_name" "fun" {
   name     = "main"
   location = "westeurope"
@@ -12,6 +40,9 @@ resource "azurerm_linux_function_app" "main" {
   storage_account_name       = azurerm_storage_account.main.name
   storage_account_access_key = azurerm_storage_account.main.primary_access_key
   service_plan_id            = azurerm_service_plan.main.id
+
+  application_insights_connection_string = azurerm_application_insights.main.connection_string
+  application_insights_key = azurerm_application_insights.main.instrumentation_key
 
   site_config {
     application_stack {
