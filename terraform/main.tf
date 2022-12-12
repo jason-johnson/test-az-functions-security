540ce3f61bb5dbba479cfd2ae39a01bec0bffca1
Jason <jason.johnson.081@gmail.com>
2022-12-12 12:47:58 +0000

    add name for subscription

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,5 +14,6 @@ provider "namep" {
 
   resource_formats = {
     azurerm_eventgrid_system_topic = "egst-#{TOKEN_1}-#{TOKEN_2}-#{SHORT_LOC}-#{NAME}"
+    azurerm_eventgrid_system_topic_event_subscription = "egsts-#{TOKEN_1}-#{TOKEN_2}-#{SHORT_LOC}-#{NAME}"
   }
 }
