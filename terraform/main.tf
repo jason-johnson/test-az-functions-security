4c434a0673a2321abb32300d8d30dce688a6651b
Jason <jason.johnson.081@gmail.com>
2022-12-14 12:12:53 +0000

    fix copy error

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,9 +5,8 @@ terraform {
 provider "azurerm" {
   features {
     resource_group {
-│       prevent_deletion_if_contains_resources = false
-│     }
-
+      prevent_deletion_if_contains_resources = false
+    }
   }
 }
 
