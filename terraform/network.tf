f97f1a051196e42284ff5cd69d9aab8d4b264f92
Jason <jason.johnson.081@gmail.com>
2022-12-19 12:08:17 +0000

    fixed typo

--- terraform/network.tf
+++ terraform/network.tf
@@ -3,7 +3,7 @@ data "namep_azure_name" "spoke" {
   type = "azurerm_virtual_network"
 }
 
-esource "azurerm_virtual_network" "main" {
+resource "azurerm_virtual_network" "main" {
   name                = data.namep_azure_name.spoke.result
   address_space       = ["10.10.0.0/16"]
   resource_group_name = azurerm_resource_group.main.name
