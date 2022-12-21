1939e9064d40a2ab5b6be015be3bb9ddb02372d5
Jason <jason.johnson.081@gmail.com>
2022-12-21 06:40:25 +0000

    move resources around a little

---
+++ terraform/event-grid.tf
@@ -0,0 +1,35 @@
+data "namep_azure_name" "egst" {
+  name     = "main"
+  location = "westeurope"
+  type     = "azurerm_eventgrid_system_topic"
+}
+
+resource "azurerm_eventgrid_system_topic" "main" {
+  name                   = data.namep_azure_name.egst.result
+  resource_group_name    = azurerm_resource_group.rg.name
+  location               = azurerm_resource_group.rg.location
+  source_arm_resource_id = azurerm_storage_account.main.id
+  topic_type             = "Microsoft.Storage.StorageAccounts"
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+data "namep_azure_name" "egsts" {
+  name     = "funsa"
+  location = "westeurope"
+  type     = "azurerm_eventgrid_system_topic_event_subscription"
+}
+
+resource "azurerm_eventgrid_system_topic_event_subscription" "main" {
+  name                = data.namep_azure_name.egsts.result
+  system_topic        = azurerm_eventgrid_system_topic.main.name
+  resource_group_name = azurerm_resource_group.rg.name
+
+  azure_function_endpoint {
+    max_events_per_batch              = 1
+    preferred_batch_size_in_kilobytes = 64
+    function_id = "${azurerm_linux_function_app.main.id}/functions/event-grid-example"
+  }
+}
