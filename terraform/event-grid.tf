703601bbf7c6c25a5a835f8aabcbedf8fbe76dae
Jason <jason.johnson.081@gmail.com>
2023-01-30 12:24:18 +0000

    reference name was wrong

--- terraform/event-grid.tf
+++ terraform/event-grid.tf
@@ -5,7 +5,7 @@ data "namep_azure_name" "egt" {
 }
 
 resource "azurerm_eventgrid_topic" "main" {
-  name                = data.namep_azure_name.result
+  name                = data.namep_azure_name.egt.result
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
